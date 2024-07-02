cff26c82490c991e54dd7b968dd668db7306b2a4
MRZ0123 <manuelmarz2004@gmail.com>
2024-07-03 00:49:48 +0200

    fixed interval

--- YAWMA_mqtt/YAWMA_mqtt.ino
+++ YAWMA_mqtt/YAWMA_mqtt.ino
@@ -43,6 +43,9 @@ AsyncMqttClient mqttClient;
 TimerHandle_t mqttReconnectTimer;
 TimerHandle_t wifiReconnectTimer;
 
+unsigned long previousMillis = 0;
+const long interval = 10000;
+
 void connectToWifi() {
   Serial.println("Connecting to Wi-Fi...");
   WiFi.begin(WIFI_SSID, WIFI_PASS);
