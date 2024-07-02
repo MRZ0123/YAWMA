c98ecd632ee166e64370edc4aa90393290ed9e3a
MRZ0123 <manuelmarz2004@gmail.com>
2024-07-03 00:25:24 +0200

    add credentials file

---
+++ YAWMA_mqtt/credentials.h
@@ -0,0 +1,13 @@
+#ifndef credentials_h
+#define credentials_h
+
+#define _ASYNC_MQTT_LOGLEVEL_ 1
+
+#define WIFI_SSID "BS4 PTR-A0"
+#define WIFI_PASS "SAE_sucks"
+
+#define MQTT_USER "wroomImAnESP"
+#define MQTT_PASS "meepmeepdoesthecar!\"§123"
+
+
+#endif    //credentials_h
